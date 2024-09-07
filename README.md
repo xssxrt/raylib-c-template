403e287b0df6280094a5433f915be046c2c0c7ff
-g <179648286+xssxrt@users.noreply.github.com>
2024-09-07 16:15:53 -0400

    add support for native builds on windows

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
+# Raylib + C template
+## Usage
+Click the button that says `Use this template`
+Select `Create a new repository`
+
 # Setup
-You will need emcc, gcc, and raylib installed.
+You will need emcc (linux), gcc, and raylib installed.
 Download `libraylib.a` and drop it in the root
 
 ## Linux
@@ -7,12 +12,12 @@ Download `libraylib.a` and drop it in the root
 `./build.sh native`
 
 ### on NixOS without flakes(wtf?)
-you may run into issues when running the game
 run the game with:
 `LD_LIBRARY_PATH="$(nix-build '<nixpkgs>' -A wayland)/lib" ./game`
 
 ## Windows
-`build.bat native`
+If the raylib installer doesn't work you will need to install the OpenCL, OpenGL, Vulkan compatibility pack from the Microsoft store
+`build.bat`
 
 ## Nix
 `nix develop`
@@ -20,6 +25,6 @@ run the game with:
 
 ## WASM
 run the build for wasm i.e `./build.sh wasm`
-serve locally with `python3 -m http.server`
+serve locally with `./serve.sh`
 navigate to `localhost:8000/game.html`
 
--- build.bat
+++ build.bat
@@ -1,53 +1,15 @@
 @echo off
-set CC=gcc
-set EMCC=emcc
-set CFLAGS=-Wall -std=c99
-set LIBS=-lraylib -lm -lpthread -ldl -lrt -lX11
+set RAYLIB_INCLUDE_DIR=C:\raylib\src
+set RAYLIB_LIB_DIR=C:\raylib\lib
+set COMPILER_FLAGS=-std=c99 -Wall -Wextra -Werror -O2
+set LINKER_FLAGS=-lraylib -lopengl32 -lgdi32 -lwinmm
+set OUTPUT_NAME=game
 set SRC=src\main.c
-set OUT_NATIVE=game.exe
-set OUT_WASM=game.html
-
-REM Function to build native version
-:build_native
-echo Building native...
-%CC% %CFLAGS% %SRC% %LIBS% -o %OUT_NATIVE%
-if %ERRORLEVEL% equ 0 (
-    echo Native build successful: %OUT_NATIVE%
+ 
+ 
+if "%1"=="clean" (
+  if exist %OUTPUT_NAME%.exe del %OUTPUT_NAME%.exe
 ) else (
-    echo Native build failed
-    exit /b 1
+  gcc %COMPILER_FLAGS% -I%RAYLIB_INCLUDE_DIR% -L%RAYLIB_LIB_DIR% %SRC% -o %OUTPUT_NAME% %LINKER_FLAGS%
 )
-goto :eof
-
-REM Function to build WebAssembly version
-:build_wasm
-echo Building WebAssembly...
-%EMCC% %SRC% -o %OUT_WASM% ^
-    -s USE_GLFW=3 ^
-    -s FULL_ES2=1 ^
-    -s ASYNCIFY ^
-    -s WASM=1 ^
-    --preload-file .
-if %ERRORLEVEL% equ 0 (
-    echo WebAssembly build successful: %OUT_WASM%
-) else (
-    echo WebAssembly build failed
-    exit /b 1
-)
-goto :eof
-
-REM Function to clean the build files
-:clean
-echo Cleaning build files...
-del /f /q %OUT_NATIVE% %OUT_WASM% *.wasm *.js *.data
-echo Clean complete
-goto :eof
-
-REM Parse command-line arguments
-if "%1"=="native" goto build_native
-if "%1"=="wasm" goto build_wasm
-if "%1"=="clean" goto clean
-
-echo Usage: build.bat {native|wasm|clean}
-exit /b 1
 
